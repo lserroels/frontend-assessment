a14a28a6f329136ced766abbc290b5e0d3a0d7b2
Luciano Serroels Demartin <lserroels@folderit.net>
2023-10-28 15:14:26 -0300

    Add price filter and auto generated options for filters

---
+++ src/components/FilterByPrice.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="filterByPriceComponent">
+        <h4>Price range:</h4>
+        <!-- <select v-model="selectedValue" @change="filterByPrice"> -->
+        <div>
+            <input type="range" v-model="selectedValue" :min="min" :max="max" />
+            <span>{{ selectedValue }}</span>
+        </div>
+    </div>
+</template>
+  
+<script>
+
+export default {
+    name: 'FilterByPriceComponent',
+    props: {
+        min: Number,
+        max: Number
+    },
+    data() {
+        return {
+        }
+    },
+    computed: {
+        selectedValue: {
+            get() {
+                return this.max; 
+            },
+            set(value) {
+                this.$emit('price-filter-event', value);
+            },
+        },
+    },
+    
+}
+</script>
+  
+<style scoped></style>
--- src/components/FilterConteiner.vue
+++ src/components/FilterConteiner.vue
@@ -1,39 +1,69 @@
 <template>
     <div id="filterConteinerComponent">
-        <FilterByBedrooms :bedsOptions=options @bedrooms-filter-event="FilterByBedrooms"></FilterByBedrooms>
-        <FilterByBathrooms :bathsOptions=options @bathrooms-filter-event="FilterByBathrooms"></FilterByBathrooms>
-        <FilterByParking :parkingOptions=options @parking-filter-event="FilterByParking"></FilterByParking>
+        <FilterByBedrooms :bedsOptions=getBedsOptions @bedrooms-filter-event="FilterByBedrooms"></FilterByBedrooms>
+        <FilterByBathrooms :bathsOptions=getBathOptions @bathrooms-filter-event="FilterByBathrooms"></FilterByBathrooms>
+        <FilterByParking :parkingOptions=getParkingOptions @parking-filter-event="FilterByParking"></FilterByParking>
+        <FilterByPrice :max=getMaxPriceRange :min=getMinPriceRange @price-filter-event="FilterByPrice"></FilterByPrice>
     </div>
 </template>
     
 <script>
 
-import FilterByBedrooms from './FilterByBedrooms.vue';
-import FilterByBathrooms from './FilterByBathrooms.vue';
-import FilterByParking from './FilterByParking.vue';
+import FilterByBedrooms from './FilterByBedrooms.vue'
+import FilterByBathrooms from './FilterByBathrooms.vue'
+import FilterByParking from './FilterByParking.vue'
+import FilterByPrice from './FilterByPrice.vue'
+
 export default {
     name: 'FilterConteinerComponent',
     props: {
-        propertiesList: []
+        propertiesList: [],
     },
     data() {
         return {
-            options: ['1', '2', '3', '4', '5', '6'],
             selectedBedrooms: '',
             selectedBathrooms: '',
             selectedParking: '',
+            selectedPrice: null
         }
     },
     components: {
         FilterByBedrooms,
         FilterByBathrooms,
-        FilterByParking
+        FilterByParking,
+        FilterByPrice
     },
     computed: {
         filteredList() {
-            return this.propertiesList.filter(prop => (this.selectedBedrooms == '' || prop.Bedrooms == this.selectedBedrooms) 
-                                                    &&(this.selectedBathrooms == '' || prop.Bathrooms == this.selectedBathrooms)
-                                                    &&(this.selectedParking == '' || prop.Parking == this.selectedParking))
+            return this.propertiesList.filter(prop => (this.selectedBedrooms == '' || prop.Bedrooms == this.selectedBedrooms)
+                && (this.selectedBathrooms == '' || prop.Bathrooms == this.selectedBathrooms)
+                && (this.selectedParking == '' || prop.Parking == this.selectedParking)
+                && (prop['Sale Price'] <= this.selectedPrice))
+        },
+        getBedsOptions() {
+            return [...new Set(this.propertiesList.map(item => item.Bedrooms).sort())];
+        },
+        getBathOptions() {
+            return [...new Set(this.propertiesList.map(item => item.Bathrooms).sort())];
+        },
+        getParkingOptions() {
+            return [...new Set(this.propertiesList.map(item => item.Parking).sort())];
+        },
+        getMinPriceRange() {
+            return this.propertiesList.reduce((min, item) => {
+                if (item['Sale Price'] < min) {
+                    return item['Sale Price'];
+                }
+                return min;
+            }, Infinity);
+        },
+        getMaxPriceRange() {
+            return  this.propertiesList.reduce((max, item) => {
+                if (item['Sale Price'] > max) {
+                    return item['Sale Price'];
+                }
+                return max;
+            }, -Infinity);
         }
     },
     watch: {
@@ -41,20 +71,23 @@ export default {
             handler(newFilteredList) {
                 return this.$emit('filter', newFilteredList)
             }
-        }
-    },
-    methods: {
-        FilterByBedrooms(bedrooms) {
-            this.selectedBedrooms = bedrooms
         },
-        FilterByBathrooms(bathrooms) {
-            this.selectedBathrooms = bathrooms
-        },
-        FilterByParking(parking) {
-            this.selectedParking = parking
-        }
     },
-}
+        methods: {
+            FilterByBedrooms(bedrooms) {
+                this.selectedBedrooms = bedrooms
+            },
+            FilterByBathrooms(bathrooms) {
+                this.selectedBathrooms = bathrooms
+            },
+            FilterByParking(parking) {
+                this.selectedParking = parking
+            },
+            FilterByPrice(price) {
+                this.selectedPrice = price
+            },
+        },
+    }
 </script>
     
 <style scoped></style>
